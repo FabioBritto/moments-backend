28d89035cf1b9423105677081d0b4ab8c8114c22
FabioBritto <fabio.tritono@gmail.com>
2026-01-03 13:19:46 -0300

    first commit with project

---
+++ src/test/java/br/com/britto/appmoments/AppmomentsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.britto.appmoments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmomentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
