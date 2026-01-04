638a6da0a231ea19abece1c60a716793a5ca7a99
FabioBritto <fabio.tritono@gmail.com>
2026-01-04 00:22:46 -0300

    feat: added validator/changed to JpaRepository

--- src/main/java/br/com/britto/appmoments/dto/EventoDTO.java
+++ src/main/java/br/com/britto/appmoments/dto/EventoDTO.java
@@ -1,15 +1,24 @@
 package br.com.britto.appmoments.dto;
 
 import br.com.britto.appmoments.model.Evento;
+import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDateTime;
 
 public record EventoDTO(
+
     String uuid,
+    @NotBlank(message = "O título é obrigatório")
     String titulo,
+
+    @NotBlank(message = "A frase é obrigatória")
     String frase,
+
+    @NotBlank(message = "A data e hora de início do evento são obrigatórios")
     LocalDateTime inicio,
     LocalDateTime fim,
+
+    @NotBlank(message = "A moldura deve ser escolhida")
     String moldura
 ) {
 
--- src/main/java/br/com/britto/appmoments/repository/EventoRepository.java
+++ src/main/java/br/com/britto/appmoments/repository/EventoRepository.java
@@ -3,12 +3,13 @@ package br.com.britto.appmoments.repository;
 import br.com.britto.appmoments.dto.EventoDTO;
 import br.com.britto.appmoments.model.Cliente;
 import br.com.britto.appmoments.model.Evento;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface EventoRepository extends CrudRepository<Evento, Integer> {
+public interface EventoRepository extends JpaRepository<Evento, Integer> {
 
     /*
     O HQL substitui nomenclatura de tabelas por nomenclatura de classes
